5bf862aaae130cb6983c16e01447b6fd05acb7b5
Ritesh25696 <chauhan.ritesh25@gmail.com>
2017-09-05 23:00:27 +0530

    Create Toggle bits given range.cpp

---
+++ Bit Manipulation/Toggle bits given range.cpp
@@ -0,0 +1,52 @@
+Given a non-negative number N and two values L and R. The problem is to toggle the bits in the range L to R in the binary representation of N, i.e, to toggle bits from the rightmost Lth bit to the rightmost Rth bit. A toggle operation flips a bit 0 to 1 and a bit 1 to 0.
+
+Input:
+First line of input contains a single integer T which denotes the number of test cases. Then T test cases follows. First line of each test case contains three space separated integers N, L and R.
+
+Output:
+For each test case , print the number obtained by toggling bits from the rightmost Lth bit to the rightmost Rth bit in binary representation of N.
+
+Constraints:
+1<=T<=100
+1<=N<=1000
+1<=L<=R
+L<=R<= Number of bits(N)
+
+Example:
+Input:
+2
+17 2 3
+50 2 5
+Output:
+23
+44
+
+
+*******************************************************************************************************************
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,l,r;
+	    cin>>n>>l>>r;
+	    int count = 1;
+	    int ans = 0;
+	    while(n != 0){
+            int bit = n&1;
+            if(count >=l && count <= r){
+                    bit ^= 1;
+            }
+            if(bit){
+	            bit<<=count-1;
+	            ans |= bit;
+	        }
+	        n>>=1;
+	        count++;
+	    }
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
