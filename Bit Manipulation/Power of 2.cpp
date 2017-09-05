deb43ad9fe8fc6b9413ee78864b13ad241cd7be8
Ritesh25696 <chauhan.ritesh25@gmail.com>
2017-09-05 23:09:10 +0530

    Create Power of 2.cpp

---
+++ Bit Manipulation/Power of 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    if(n == 0)cout<<"NO"<<endl;
+	    else{
+	        if((n & (n-1)) == 0)cout<<"YES"<<endl;
+	        else cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
